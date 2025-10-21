3729c24eb2391e3baff45601b97784ee971e9bce
Luaevente <ooochs.lukas.privat@gmail.com>
2025-10-21 13:40:32 +0200

    Karten-Funktionalität für Benutzerstandort erweitert

--- app/src/main/java/com/example/osnalokal/MapActivity.java
+++ app/src/main/java/com/example/osnalokal/MapActivity.java
@@ -18,25 +18,15 @@ public class MapActivity extends AppCompatActivity {
 
         webView = findViewById(R.id.mapWebView);
 
-        // WebSettings holen
         WebSettings webSettings = webView.getSettings();
-
-        // 1. JavaScript aktivieren (SEHR WICHTIG für Leaflet)
         webSettings.setJavaScriptEnabled(true);
-
-        // 2. DOM Storage aktivieren (wird oft von Karten-Libs gebraucht)
         webSettings.setDomStorageEnabled(true);
-
-        // 3. Zugriff auf lokale Dateien erlauben (DAMIT WIR map.html LADEN KÖNNEN)
         webSettings.setAllowFileAccess(true);
         webSettings.setAllowFileAccessFromFileURLs(true);
         webSettings.setAllowUniversalAccessFromFileURLs(true);
 
-        // Verhindert, dass Links im Systembrowser geöffnet werden
         webView.setWebViewClient(new WebViewClient());
 
-        // 4. Unsere lokale map.html laden
-        // "file:///android_asset/" ist der Pfad zu deinem assets-Ordner
         webView.loadUrl("file:///android_asset/map.html");
     }
 }
